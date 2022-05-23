71d1660f8da5b8b7074797a10044801bd0440fea
prmudali <104631283+prmudali@users.noreply.github.com>
2022-05-23 06:51:02 -0700

    Add files via upload

---
+++ SQL/Activities/Customer.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE Customers (
+    CustomerID int,
+    CustomerName varchar2(52),
+    ContactName varchar2(32),
+    Address varchar2(255),
+    City varchar2(50),
+    PostalCode varchar2(20),
+    Country varchar2(20)
+); 
---
+++ SQL/Activities/Salesman.sql
@@ -0,0 +1,8 @@
+
+Activity 1:
+CREATE TABLE salesman (
+    salesman_id int,
+    salesman_name varchar2(32),
+    salesman_city varchar2(32),
+    commission int
+);
